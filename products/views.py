e21d089f013833702fe9e71ccc17ac0c537134b2
MarisKX <maris.kurpnieks@gmail.com>
2023-02-09 19:45:21 +0000

    Created search funstionality in products view

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 # General imports
 from django.shortcuts import render, get_object_or_404, reverse
 from django.http import JsonResponse, HttpResponseRedirect
+from django.db.models import Q
 # PDF imports
 from django.http import FileResponse
 import io
@@ -24,10 +25,44 @@ def all_products(request):
 
     products = Product.objects.all().order_by("display_name")
     all_categories = Category.objects.all()
+    all_subcategories = SubCategory.objects.all()
+
+    def is_ajax(request):
+        return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+
+    if is_ajax(request):
+        category = request.GET.get('category')
+        if category is not None:
+            subcategories = SubCategory.objects.filter(
+                category=category).values_list('display_name')
+            subcategories_id = SubCategory.objects.filter(
+                category=category).values_list('id')
+            return JsonResponse({
+                "subcategories_to_return": list(subcategories),
+                "subcategories_id_to_return": list(subcategories_id),
+                })
+
+    if request.GET:
+        if 'category' and 'subcategory' in request.GET:
+            query_filter_category = request.GET['category']
+            query_filter_subcategory = request.GET['subcategory']
+
+            queries = Q(
+                category__id__icontains=query_filter_category) & Q(
+                subcategory__id__icontains=query_filter_subcategory)
+            products = products.filter(queries)
+
+        if 'category' in request.GET:
+            query_filter_category = request.GET['category']
+
+            queries = Q(
+                category__id__icontains=query_filter_category)
+            products = products.filter(queries)
 
     context = {
         'products': products,
         'all_categories': all_categories,
+        'all_subcategories': all_subcategories,
     }
 
     return render(request, 'products/products.html', context)
---
+++ static/js/productSearchHandler.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    let initList = $(".subcategory-search-list")
+        if (initList[0].hasChildNodes()) {
+            initList.empty();
+            initList.attr('disabled', true)
+            $(".subcategory-search-list").append('<option value="">Select All</option>');
+    };
+    $(document.body).on('change',".category-search-list",function(){
+        let value = $(this).val()
+        console.log(value)
+        let list = $(".subcategory-search-list")
+        if (list[0].hasChildNodes()) {
+            list.empty()
+        };
+        $(".subcategory-search-list").append('<option value="">Select All</option>');
+
+        $.ajax({
+            url: '',
+            type: 'get',
+            data: {
+                category: value
+            },
+            success: function(response){
+                console.log(response)
+                let subcategories = response.subcategories_to_return
+                let subcategories_id = response.subcategories_id_to_return
+                $.each(subcategories,function(key, value){
+                    $(".subcategory-search-list").append('<option class="subcategory-value">' + value + '</option>');
+                    $(".subcategory-search-list").attr('disabled', false)
+                });
+                let count = 0
+                $.each(subcategories_id,function(key, value){
+                    $(".subcategory-value").eq(count).attr('value', value);
+                    count +=1
+                });
+            }
+        });
+    });
+});
