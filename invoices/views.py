0c5d75ffc915e0feaa2cdcfe7e14e74605f3cb88
MarisKX <maris.kurpnieks@gmail.com>
2023-03-09 10:40:45 +0000

    Finished bulk retail sales

--- invoices/views.py
+++ invoices/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404, reverse
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.core.exceptions import ObjectDoesNotExist
+from django.http import JsonResponse, HttpResponseRedirect
 
 # Time imports
 from datetime import *
@@ -17,6 +18,10 @@ from companies.models import Company
 from bank.models import BankAccountEntry, BankAccount
 from products.models import Product
 from citizens.models import Citizen
+from warehouses.models import Warehouse
+
+# Custom functions import
+from home.today_calculation import today_calc
 
 
 # All Invoices View
@@ -122,18 +127,65 @@ def bulk_retail_sale(request):
         return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
 
     if is_ajax(request):
+        retailerCheck = request.GET.get('retailerCheck')
+        if retailerCheck is not None:
+
+            company = get_object_or_404(Company, registration_number=retailerCheck)
+            retailer_warehouses_value = Warehouse.objects.filter(
+                company=company,
+                internal_warehouse=False
+                ).order_by("display_name").values_list('name')
+            retailer_warehouses = Warehouse.objects.filter(
+                company=company,
+                internal_warehouse=False
+                ).order_by("display_name").values_list('display_name')
+            print(retailer_warehouses)
+            return JsonResponse({
+                "retailer_warehouses_value": list(retailer_warehouses_value),
+                "retailer_warehouses": list(retailer_warehouses),
+                })
+
         bulk_retailer = request.GET.get('retailer')
+        bulk_retailer_warehouse = request.GET.get('retailerWarehouse')
         bulk_product = request.GET.get('product')
-        bulk_qty = request.GET.get('qty')
-        if retailer and product and qty is not None:
+        bulk_qty = int(request.GET.get('qty'))
+        bulk_price = float(request.GET.get('price'))
+
+        if bulk_retailer and bulk_product and bulk_qty is not None:
+
             all_citizens = Citizen.objects.all()
             retailer = get_object_or_404(Company, registration_number=bulk_retailer)
+            retailerWarehouse = get_object_or_404(Warehouse, name=bulk_retailer_warehouse)
+            product = get_object_or_404(Product, name=bulk_product)
+            today = today_calc()
             for citizen in all_citizens:
                 RetailSale.objects.create(
-                        retailer=retailer,
-                        description=f"" + instance.report_number + ", BTW",
-                        amount_plus=instance.amount,
+                    retailer=retailer,
+                    retailer_warehouse=retailerWarehouse,
+                    customer=citizen,
+                    date=today,
+                    retail_sale_paid=False,
+                    retail_sale_paid_confirmed=False,
                 )
+                print("retail sale created")
+                created_retail_sale = RetailSale.objects.filter(
+                    retailer=retailer,
+                    retailer_warehouse=retailerWarehouse,
+                    customer=citizen,
+                    date=today,
+                ).last()
+                print(f"Last retail sale found " + created_retail_sale.retail_sale_number + "")
+                print(type(bulk_qty))
+                print(type(bulk_price))
+                RetailSaleItem.objects.create(
+                    retail_sale=created_retail_sale,
+                    product=product,
+                    quantity=bulk_qty,
+                    price=bulk_price,
+                )
+                print("Created Retail Sale Items")
+                created_retail_sale.retail_sale_paid = True
+                created_retail_sale.save()
 
     context = {
         'all_companies': all_companies,
--- static/js/createBulkRetailSale.js
+++ static/js/createBulkRetailSale.js
@@ -1,31 +1,52 @@
 $(document).ready(function(){
+    $(document.body).on('change',"#retailer-select",function(){
+        let retailerCheck = $(this).val()
+        console.log(retailerCheck)
+        $.ajax({
+            url: '',
+            type: 'get',
+            data: {
+                retailerCheck: retailerCheck,
+            },
+            success: function(response){
+                let retailer_warehouses = response.retailer_warehouses
+                let retailer_warehouses_value = response.retailer_warehouses_value
+                $.each(retailer_warehouses,function(key, value){
+                    $("#retailer-warehouse-select").append(
+                        '<option class="warehouse-select" value=' + value + '>' + value + '</option>');
+                });
+                let count = 0
+                $.each(retailer_warehouses_value,function(key, value){
+                    $(".warehouse-select").eq(count).attr('value', value);
+                    count +=1
+                });
+            }
+        });
+    });
     $("#create-bulk-retail-sale").click(function(){
         console.log("Button click received")
         let retailer = $("#retailer-select").val();
+        let retailerWarehouse = $("#retailer-warehouse-select").val();
         let product = $("#product-select").val();
         let qty = $("#qty-select").val();
+        let price = +$("input[name='price']").val();
         console.log(retailer)
+        console.log(retailerWarehouse)
         console.log(product)
         console.log(qty)
+        console.log(price)
         $.ajax({
             url: '',
             type: 'get',
             data: {
                 retailer: retailer,
+                retailerWarehouse: retailerWarehouse,
                 product: product,
                 qty: qty,
+                price: price,
             },
             success: function(response){
-                let subcategories = response.subcategories_to_return
-                let subcategories_id = response.subcategories_id_to_return
-                $.each(subcategories,function(key, value){
-                    $("#id_subcategory").append('<option class="subcategory-value">' + value + '</option>');
-                });
-                let count = 0
-                $.each(subcategories_id,function(key, value){
-                    $(".subcategory-value").eq(count).attr('value', value);
-                    count +=1
-                });
+                console.log("Success")
             }
         });
     });
